db4ddee36678101e2a18c618df395f3bdeae9841
Kenneth Portillo <portillokenneth@gmail.com>
2019-08-22 15:40:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -10,19 +10,19 @@ def alan_kay_is_known_for
 	
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
+       # :grace_hopper => {
+         # :known_for => "COBOL",
+        #  :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
+        #:alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
         }
-     }
+        #:dennis_ritchie => {
+        #  :known_for => "Unix",
+        #  :languages => ["C"]
+        #}
+     #}
 end
 
 def dennis_ritchies_language
